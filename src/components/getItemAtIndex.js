bde6f59b9597138e7b9cbecbae8bdba2f7fbbd45
John Rees <john@bitsushi.com>
2017-02-26 14:46:54 +0000

    changed getitematindex params from functions to constants

--- src/components/getItemAtIndex.js
+++ src/components/getItemAtIndex.js
@@ -1,5 +1,5 @@
-const getItemAtIndex = ({ LIST = () => ([]), INDEX = () => (0)} = {}) => {
-  return { ITEM: LIST()[INDEX()] }
+const getItemAtIndex = ({ LIST = [], INDEX = 0 } = {}) => {
+  return { ITEM: LIST[INDEX] }
 }
 
 export default getItemAtIndex
